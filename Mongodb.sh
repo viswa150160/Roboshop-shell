8172864ff5a5683912c4b624b02f28fd2bf7f79b
viswa150160 <viswa160150@gmail.com>
2024-04-14 10:14:16 +0530

    created mongodb

---
+++ Mongodb.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DATE=$(Date +%F)
+LOGDIR=/tmp
+SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$0_$DATE.Log
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+# Checking here whether user has ROOT access or NOT
+
+if [$USERID -ne 0];
+    then 
+        echo -e "$R ERROR:: please run the script with root access $N"
+        exist 1
+fi
+
+VALIDATE() {
+   if [$1 -ne 0];
+      then 
+         echo -e "$2...$R FAILURE $N"
+         exit 1
+    else
+         echo -e "$2...$G SUCCESS $N"
+   fi 
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copied mongodb repo into yum.repos.d"
+
+yum install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "install mongodb"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "enable mongod"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "start mongod"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Edit mongod.conf"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? " restart mongod"
+
+
+
+
